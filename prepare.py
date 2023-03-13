d994f696f46eae1be5785a111728871f94981d18
heywood95 <adam.heywood8@gmail.com>
2023-03-13 11:12:27 -0500

    adding prepare

---
+++ prepare.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+from typing import Dict, List, Optional, Union, cast
+from bs4 import BeautifulSoup
+import os
+from sklearn.model_selection import train_test_split
+import requests
+import nltk
+import unicodedata
+import re
+
+
+
+
+def prep_bills(df):
+    '''Prepares acquired world government data for exploration'''
+    
+    new_df = df.loc[df['bill_text'].str.len() >= 35]
+    
+    return new_df
+
+def clean_text(text, extra_stopwords=[]):
+
+    '''
+    This function takes in the words and cleans it, and returns the words that have been 
+    lemmatized.
+    '''
+
+    bills2 = bill_df.loc[bill_df['title'].str.len() >= 35]
+    wnl = nltk.stem.WordNetLemmatizer()
+    stopwords = nltk.corpus.stopwords.words('english') + extra_stopwords
+    clean_text = (unicodedata.normalize('NFKD', text)
+                   .encode('ascii', 'ignore')
+                   .decode('utf-8', 'ignore')
+                   .lower())
+    words = re.sub(r'[^\w\s___]', '', clean_text).split()
+    words = re.sub(r'_', '',' '.join(words)).split(' ')
+    words = [w for w in words if len(w)<25]
+    return [wnl.lemmatize(word) for word in words if word not in stopwords]
+
+def split_data(df, target):
+    
+    '''
+    Splits a df into a train, validate, and test set. 
+    target is the feature you will predict
+    '''
+    full = df
+    train_validate, test = train_test_split(df, train_size =.8, random_state = 21)
+    train, validate = train_test_split(train_validate, train_size = .7, random_state = 21)
+    X_train = train.drop(columns=target)
+    y_train = train[target]
+    X_val = validate.drop(columns=target)
+    y_val = validate[target]
+    X_test = test.drop(columns=target)
+    y_test = test[target]
+    
+    
+    return train, X_train, y_train, X_val, y_val, X_test, y_test
+
+
+# In[ ]:
+
+
+
+
